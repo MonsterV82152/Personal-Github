51b182b10e0283bce26d7d41b57912c255282527
MonsterV82152 <eddyliang193@gmail.com>
2025-07-03 20:00:11 -0400

    some file name changes

--- compSci/practiceJava/playground/src/Playground.java
+++ compSci/practiceJava/playground/src/Playground.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Playground {
     public static void main(String[] args) {
-        System.out.printf("%3.2f", 123123.2341924);
-        int[][] b = {{0,0,0},{0,0,0}};
-        Bases a = new Bases(b);
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter r/c+NUM:");
+        char[] b = scan.nextLine().toCharArray();
+        int num = 0;
+        for (int i = 1; i < b.length; i++) {
+            num += ((int) b[i] - 48) * Math.pow(10, b.length - i - 1);
+        }
+        if (b[0] == 'r') {
+            System.out.println(num * 4);
+        } else if (b[0] == 'c') {
+            System.out.printf("%.3f \n", num * 2 * Math.PI);
+        }
+        scan.close();
     }
 }
-
-
-
-
