8637e8cbd39feb4fc1fc3b6d519bd87ac80fc2bc
MonsterV82152 <eddyliang193@gmail.com>
2025-07-14 21:23:44 -0400

    S5 attempts

--- compSci/practiceJava/playground/src/CCC24S5.java
+++ compSci/practiceJava/playground/src/CCC24S5.java
@@ -1,21 +1,54 @@
+/**
+ * @author Eddy
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
 public class CCC24S5 {
     static int sum = 0;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         scan.nextLine();
-        ArrayList<char[]> grid = new ArrayList<char[]>();
-        grid.add(scan.nextLine().split(" "));
-        grid.add(scan.nextLine().split(" "));
-        for (char[] a : grid) {
-            for (char b : a) {
-                sum += (int) b - 48;
-            }
+        // Old version of the code made by Eddy
+        /*
+
+        ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < 2; i++) {
+        String[] inp = scan.nextLine().split(" ");
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        for (String a : inp) {
+        row.add(Integer.parseInt(a));
+        }
+        grid.add(row);
         }
 
-
-
+        */
         
+
+        // More consise version made by ChatGPT using Arrays.stream and List instead of ArrayList.
+        List<List<Integer>> grid = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            List<Integer> row = Arrays.stream(scan.nextLine().split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            grid.add(row);
+        }
+
+        boolean[][] visited = { new boolean[n], new boolean[n] };
+        
+        Queue<int[]> queue = new LinkedList<>();
+
+        while (!queue.isEmpty()) {
+
+        }
     }
 }
