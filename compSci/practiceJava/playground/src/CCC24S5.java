6795c8d8f4ecd53327f9e6b1d2192837b7b74aaf
MonsterV82152 <eddyliang193@gmail.com>
2025-07-15 14:38:52 -0400

    S5 Attempt #2

--- compSci/practiceJava/playground/src/CCC24S5.java
+++ compSci/practiceJava/playground/src/CCC24S5.java
@@ -1,3 +1,4 @@
+
 /**
  * @author Eddy
  */
@@ -10,31 +11,34 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-
 public class CCC24S5 {
-    static int sum = 0;
+    static int sum;
+    static boolean[][] visited;;
+    static int total;
+    static int n;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        n = scan.nextInt();
+        visited = new boolean[2][n];
         scan.nextLine();
         // Old version of the code made by Eddy
         /*
+         * 
+         * ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
+         * for (int i = 0; i < 2; i++) {
+         * String[] inp = scan.nextLine().split(" ");
+         * ArrayList<Integer> row = new ArrayList<Integer>();
+         * for (String a : inp) {
+         * row.add(Integer.parseInt(a));
+         * }
+         * grid.add(row);
+         * }
+         * 
+         */
 
-        ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < 2; i++) {
-        String[] inp = scan.nextLine().split(" ");
-        ArrayList<Integer> row = new ArrayList<Integer>();
-        for (String a : inp) {
-        row.add(Integer.parseInt(a));
-        }
-        grid.add(row);
-        }
-
-        */
-        
-
-        // More consise version made by ChatGPT using Arrays.stream and List instead of ArrayList.
+        // More consise version made by ChatGPT using Arrays.stream and List instead of
+        // ArrayList.
         List<List<Integer>> grid = new ArrayList<>();
         for (int i = 0; i < 2; i++) {
             List<Integer> row = Arrays.stream(scan.nextLine().split(" "))
@@ -43,12 +47,49 @@ public class CCC24S5 {
             grid.add(row);
         }
 
-        boolean[][] visited = { new boolean[n], new boolean[n] };
-        
-        Queue<int[]> queue = new LinkedList<>();
-
-        while (!queue.isEmpty()) {
-
+        for (List<Integer> row : grid) {
+            for (int value : row) {
+                sum += value;
+            }
         }
+        total = n * 2;
+        System.out.println(findChunks(grid, new boolean[2][n], 0, 0, 0, new int[]{0,0}, new boolean[2][n]));
+    }
+
+    private static int findChunks(List<List<Integer>> grid, boolean[][] visited, int currentSum, int currentTotal,
+            int chunks, int[] pos, boolean[][] tempVisited) {
+
+        if (pos[1] >= 0 && pos[1] < n && !tempVisited[pos[0]][pos[1]] && !visited[pos[0]][pos[1]]) {
+            tempVisited[pos[0]][pos[1]] = true;
+            currentSum += grid.get(pos[0]).get(pos[1]);
+            currentTotal++;
+            if (currentSum * total == sum * currentTotal) {
+                for (int i = 0; i < 2; i++) {
+                    for (int m = 0; m < n; m++) {
+                        visited[i][m] = visited[i][m] || tempVisited[i][m];
+                    }
+                }
+                for (int i = 0; i < visited.length; i++) {
+                    for (int m = 0; m < visited[i].length; m++) {
+                        if (!visited[i][m]) {
+                            return findChunks(grid, visited, 0, 0, chunks+1, new int[]{i, m}, new boolean[2][n]);
+                        }
+                    }
+                }
+                return chunks+1;
+            }
+            int[][] neighbors = new int[][]{{pos[0]^1, pos[1]},{pos[0], pos[1]+1},{pos[0], pos[1]-1}};
+            int highest = 0;
+            for (int[] i : neighbors) {
+                int temp = findChunks(grid, visited, currentSum, currentTotal, chunks, i, tempVisited);
+                if (temp > highest) {
+                    highest = temp;
+                }
+            }
+            return highest;
+        }
+
+        return 1;
+
     }
 }
---
+++ compSci/practiceJava/playground/src/longPrint.java
@@ -0,0 +1,30 @@
+/**
+ * @author Me
+ * @date Jul 15
+ * 
+ */
+import java.lang.reflect.Executable;
+import java.util.Scanner;
+
+import javax.management.timer.Timer;
+/**
+ * @author me
+ */
+public class LongPrint {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        for (String input = ""; !input.equals("quit"); input = scan.nextLine()) {
+            String current = "";
+            for (char i : input.toCharArray()) {
+                for (char n = '0'; n < 'z'; n++) {
+                    System.out.println(current+n);
+                    Thread.sleep(10);
+                    if (n == i) {
+                        current = current + n;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
