d375e7326021bf404463a6781f734b965a5213d0
Eddy Liang <eddyliang193@gmail.com>
2025-10-17 10:35:10 -0400

    quicksort

--- ICS4U1/playground/src/App.java
+++ ICS4U1/playground/src/App.java
@@ -1,5 +1,44 @@
 public class App {
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // set the last element as pivot
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) { // iterate through all elements
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot) {
+                i++; // increment index of smaller element
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int n = arr.length;
+        quickSort(arr, 0, n - 1);
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
