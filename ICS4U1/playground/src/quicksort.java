d73e0741ca03bf2df385ea3511a8e5aaee9eb666
Eddy Liang <eddyliang193@gmail.com>
2025-10-20 21:54:57 -0400

    john's quicksort

--- ICS4U1/playground/src/quicksort.java
+++ ICS4U1/playground/src/quicksort.java
@@ -1,41 +1,50 @@
 public class Quicksort {
+    
+    static int[] swap(int[] arr, int a, int b) {
+        // swap arr[i+1] and arr[high]
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        return arr;
+    }
+    
+    //returns int of pivot's final position
     static int partition(int[] arr, int low, int high) {
         int pivot = arr[high]; // set the last element as pivot
-        int i = (low - 1); // index of smaller element
-        for (int j = low; j < high; j++) { // iterate through all elements
-            // If current element is smaller than the pivot
+        int i = (low - 1); // index of where to place next smaller element than pivot
+        for (int j = low; j < high; j++) {
+            //check if element is smaller than the pivot
             if (arr[j] < pivot) {
-                i++; // increment index of smaller element
+                i++;
 
-                // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                //place the element in the correct position
+                arr = swap(arr, i, j);
             }
         }
 
-        // swap arr[i+1] and arr[high]
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
+        //place the pivot in the correct position
+        arr = swap(arr, i+1, high);
 
+        //return the pivot's position in the partitioned list
         return i + 1;
     }
 
     // Recursive quicksort function
     static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
-            int pi = partition(arr, low, high);
+            int splitIndex = partition(arr, low, high);
 
             // Recursively sort elements before partition and after partition
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            quickSort(arr, low, splitIndex - 1);
+            quickSort(arr, splitIndex + 1, high);
         }
     }
 
     public static void main(String[] args) throws Exception {
-        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int[] arr = { 10, 7, 8, 9, 1, 5, 3,6,1,4,45,234,7,21,34,7,234,1,65,45,2,23,41 };
         int n = arr.length;
+        
+        //sort the array
         quickSort(arr, 0, n - 1);
         System.out.println("Sorted array: ");
         for (int i = 0; i < n; i++) {
